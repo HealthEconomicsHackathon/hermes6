fed611d25dd1b247039c08c787e3a788b78cda39
seabbs <s.e.abbott12@gmail.com>
2019-11-07 12:24:56 +0000

    functional approach up to single model run

--- R/markov_seabbs.R
+++ R/markov_seabbs.R
@@ -1,5 +1,6 @@
 library(tibble)
-
+library(purrr)
+library(magrittr)
 
 # Transitions -------------------------------------------------------------
 # 1. Specify transition matrices for each intervention
@@ -16,8 +17,8 @@ soc_transition <- function() {
 
 # Intervention - Soc with website
 # Depends on Soc
-sco_with_website_transition <- function(baseline) {
-  baseline[1, ] <- VGAM::rdiric(n.samples,c(85,15))
+soc_with_website_transition <- function(baseline = NULL) {
+  baseline[1, ] <- VGAM::rdiric(1,c(85,15))
   
   return(baseline)
 }
@@ -27,32 +28,42 @@ sco_with_website_transition <- function(baseline) {
 soc_trans_sample <- soc_transition()
 soc_trans_sample
 
-soc_with_website_trans_sample <- sco_with_website_transition(soc_trans_sample)
+soc_with_website_trans_sample <- soc_with_website_transition(soc_trans_sample)
 soc_with_website_trans_sample
 
 
 # Qualies -----------------------------------------------------------------
-# 2. Specify qualy costs per intervention (random sampling)
+# 2. Specify qaly costs per intervention (random sampling)
 
 
-qualy <- function(samples = 1) {
-  smoking <- rnorm(1, mean = 0.95,sd = 0.01) / 2
-  not_smoking <- 1 / 2
+qalys <- function() {
+  qaly <- function(samples = 1) {
+    smoking <- rnorm(1, mean = 0.95,sd = 0.01) / 2
+    not_smoking <- 1 / 2
+    
+    out <- c(smoking, not_smoking)
+    names(out) <- c("Smoking", "Not smoking")
+    
+    return(out)
+    
+  }
   
-  out <- c(smoking, not_smoking)
-  names(out) <- c("Smoking", "Not smoking")
+  soc <- qaly()
+  soc_with_website <- soc
+  
+  out <- list(soc, soc_with_website)
+  names(out) <- list("SoC", "Soc with Website")
   
   return(out)
-  
 }
 
-qualy()
+qalys()
 
 
 # Costs -------------------------------------------------------------------
 # 3. Specify costs per intervention (random sampling)
 
-costs <- function(samples = 1) {
+intervention_costs <- function(samples = 1) {
   soc <- 0
   soc_with_website <- 50
   
@@ -62,28 +73,152 @@ costs <- function(samples = 1) {
   return(out)
 }
 
-costs()
+intervention_costs()
+
+
+state_costs <- function(samples = 1) {
+  state_cost <- function(samples = 1) {
+    smoking <- 0
+    not_smoking <- 0
+    
+    out <- c(smoking, not_smoking)
+    names(out) <- c("Smoking", "Not smoking")
+    
+    return(out)
+    
+  }
+  
+  soc <- state_cost()
+  soc_with_website <- soc
+  
+  out <- list(soc, soc_with_website)
+  names(out) <- list("SoC", "Soc with Website")
+  return(out)
+}
+
+state_costs()
 
 
 
 # Cohort ------------------------------------------------------------------
 #4. Define cohort
 
-cohort <- function() {
-  smoking <- 1
-  not_smoking <- 0
+cohorts <- function() {
   
-  out <- c(smoking, not_smoking)
-  names(out) <- c("Smoking", "Not smoking")
+  cohort <- function() {
+    smoking <- 1
+    not_smoking <- 0
+    
+    out <- matrix(c(smoking, not_smoking), ncol = 2)
+    colnames(out) <- c("Smoking", "Not smoking")
+    
+    return(out)
+  }
+  
+  soc <- cohort()
+  soc_with_website <- soc
+  
+  out <- list(soc, soc_with_website)
+  names(out) <- list("SoC", "Soc with Website")
   
   return(out)
 }
 
-cohort()
+cohorts()
+
+# Set up single sample -----------------------------------------------------
+#5. Set up single sample
+
+## Set up sampling function
+single_sample <- function(transitions = NULL, state_costs = NULL, 
+                          intervention_costs = NULL, cohorts = NULL, 
+                          qalys = NULL) {
+  
+  #sample baseline transition matrix
+  baseline <- transitions[[1]]()
+  
+  #sample all interventions depending on baseline
+  interventions <- purrr::map(2:length(transitions), ~ transitions[[.]](baseline))
+
+  #update transitions as a single sample
+  transitions[[1]] <- baseline
+  transitions[-1] <- interventions
+  
+  sample <- tibble::tibble(intervention = names(transitions), 
+                           transition = transitions, 
+                           state_cost = state_costs(), 
+                           intervention_cost = intervention_costs(),
+                           cohort = cohorts(),
+                           qalys = qalys()
+                           )
+  
+}
+
+# List of all transitions (named), all subsequent transitions from the first must take the first transition as
+# an input
+transitions_list <- list(soc_transition, 
+                    soc_with_website_transition)
+
+names(transitions_list) <- c("SoC", "Soc with Website")
+
+test_sample <- single_sample(transitions = transitions_list,
+                             state_costs = state_costs,
+                             intervention_costs = intervention_costs,
+                             cohorts = cohorts, qalys = qalys)
 
 
-# Set up and sampling -----------------------------------------------------
-#5. Set up sampling
+
+# Run the model -----------------------------------------------------------
+# Run the model a single time
+
+run_markov <- function(transition = NULL, cohort = NULL, state_cost = NULL, 
+                       intervention_cost = NULL, qalys = NULL, duration = NULL,
+                       discount = NULL) {
+  
+  ## Preallocate
+  sim <- matrix(NA, nrow = duration, ncol = nrow(transition))
+  colnames(sim) <- colnames(transition)
+  
+  ## Assign initial pop
+  sim[1, ] <- cohort
+  
+  ##Loop over the rest of the model 
+  for (i in 2:duration) {
+    sim[i, ] <- sim[i - 1, ] %*% transition
+  }
+  
+  ## Discounting
+  discounting <-  (1 / 1.035)^(0:(duration - 1))
+  
+  ##Total costs per cycle
+  total_costs_cycle <- (sim %*% state_cost) * discounting
+  
+  ##Total QALYs per cycle
+  total_qalys <- (sim %*% qalys) * discounting
+  
+  ## Overall costs
+  total_costs <- total_costs_cycle + intervention_cost
+  
+  out <- tibble::tibble(total_costs = list(total_costs), total_qalys = list(total_qalys))
+  
+  return(out)
+}
+
+## Test model run
+test_sim <- run_markov(transition = test_sample$transition[[1]],
+                       cohort = test_sample$cohort[[1]],
+                       state_cost = test_sample$state_cost[[1]], 
+                       intervention_cost = test_sample$intervention_cost[[1]], 
+                       qalys = test_sample$qalys[[1]], 
+                       duration = 10,
+                       discount = 1.035)
+
+
+# Analyse model -----------------------------------------------------------
+
+analyse_model <- function() {
+  
+}
 
 # Smoking Cessation Markov model
 # Howard Thom 14-June-2019
@@ -105,146 +240,6 @@ markov_seabbs <- function(n.cycles = 100, n.samples = 10000) {
   set.seed(14143)
   
 
-  #############################################################################
-  ## Input parameters #########################################################
-  #############################################################################
-  
-  # The transition matrix is a 2x2 matrix
-  # Rows sum to 1
-  # Top left entry is transition probability from smoking to smoking
-  # Top right is transition probability from smoking to not smoking
-  # Bottom left is transition probability from not smoking to smoking
-  # Bottom right is transition probability from not smoking to not smoking
-  
-  # There is one transition matrix for each reatment option and each PSA sample
-  # Store them in an array with (before filling in below) NA entries
-  transition.matrices<-array(dim=c(n.treatments,n.samples,n.states,n.states),
-                             dimnames=list(treatment.names,NULL,state.names,state.names))
-  
-  # First the transition matrix for Standard of Care with website
-  # Transitions from smoking 
-  transition.matrices["SoC with website",,"Smoking",]<-VGAM::rdiric(n.samples,c(85,15))
-  # Transitions from not smoking
-  transition.matrices["SoC with website",,"Not smoking",]<-VGAM::rdiric(n.samples,c(8,92))
-  
-  # Second the transition matrix for Standard of Care
-  # Transitions from smoking 
-  transition.matrices["SoC",,"Smoking",]<-VGAM::rdiric(n.samples,c(88,12))
-  # Transitions from not smoking
-  # These should be the same as the transition probabilities from not smoking for SoC with website
-  # as the website has no impact on probability of relapse
-  transition.matrices["SoC",,"Not smoking",]<-transition.matrices["SoC with website",,"Not smoking",]
-  
-  # Now define the QALYS associated with the states per cycle
-  # There is one for each PSA sample and each state
-  # Store in an NA array and then fill in below
-  state.qalys<-array(dim=c(n.samples, n.states),dimnames=list(NULL,state.names))
-  
-  # QALY associated with 1-year in the smoking state is Normal(mean=0.95, SD=0.01)
-  # Divide by 2 as cycle length is 6 months
-  state.qalys[,"Smoking"]<-rnorm(n.samples,mean=0.95,sd=0.01)/2
-  
-  # QALY associated with 1-year in the not smoking state is 1 (no uncertainty)
-  # So all PSA samples have the same value
-  # Again divide by 2 as cycle length is 6 months
-  state.qalys[,"Not smoking"]<-1/2
-  
-  # And finally define the state costs
-  # These are all zero as the only cost is a one-off subscription fee of ?50
-  # to the smoking cessation website
-  state.costs<-array(0,dim=c(n.samples, n.states),dimnames=list(NULL,state.names))
-  
-  # Define the treatment costs
-  # One for each PSA sample and each treatment
-  # Treatment costs are actually fixed but this allows flexibility if we
-  # want to include uncertainty/randomness in the cost
-  treatment.costs<-array(dim=c(n.treatments,n.samples),dimnames=list(treatment.names,NULL))
-  
-  # Cost of the smoking cessation website is a one-off subscription fee of ?50
-  treatment.costs["SoC with website",]<-50
-  # Zero cost for standard of care
-  treatment.costs["SoC",]<-0
-  
-  #############################################################################
-  ## Simulation ###############################################################
-  #############################################################################
-  
-  # Build an array to store the cohort vector at each cycle
-  # Each cohort vector has 2 (=n.states) elements: probability of being in smoking state,
-  # and probability of being in the not smoking state
-  # There is one cohort vector for each treatment, for each PSA sample, for each cycle.
-  cohort.vectors<-array(dim=c(n.treatments,n.samples,n.cycles,n.states),
-                        dimnames=list(treatment.names,NULL,NULL,state.names))
-  
-  # Assume that everyone starts in the smoking state no matter the treatment
-  cohort.vectors[,,1,"Smoking"]<-1
-  cohort.vectors[,,1,"Not smoking"]<-0
-  
-  # Build an array to store the costs and QALYs accrued per cycle
-  # One for each treatment, for each PSA sample, for each cycle
-  # These will be filled in below in the main model code
-  # Then discounted and summed to contribute to total costs and total QALYs
-  cycle.costs<-array(dim=c(n.treatments,n.samples,n.cycles),
-                     dimnames=list(treatment.names,NULL,NULL))
-  cycle.qalys<-array(dim=c(n.treatments,n.samples,n.cycles),
-                     dimnames=list(treatment.names,NULL,NULL))
-  
-  # Build arrays to store the total costs and total QALYs
-  # There is one for each treatment and each PSA sample
-  # These are filled in below using cycle.costs, 
-  # treatment.costs, and cycle.qalys
-  total.costs<-array(dim=c(n.treatments,n.samples),
-                     dimnames=list(treatment.names,NULL))
-  total.qalys<-array(dim=c(n.treatments,n.samples),
-                     dimnames=list(treatment.names,NULL))
-  
-  
-  # The remainder of the cohort.vectors will be filled in by Markov updating below
-  
-  # Main model code
-  # Loop over the treatment options
-  for(i.treatment in 1:n.treatments)
-  {
-    # Loop over the PSA samples
-    for(i.sample in 1:n.samples)
-    {
-      # Loop over the cycles
-      # Cycle 1 is already defined so only need to update cycles 2:n.cycles
-      for(i.cycle in 2:n.cycles)
-      {
-        # Markov update
-        # Multiply previous cycle's cohort vector by transition matrix
-        # i.e. pi_j = pi_(j-1)*P
-        cohort.vectors[i.treatment,i.sample,i.cycle,]<-
-          cohort.vectors[i.treatment,i.sample,i.cycle-1,]%*%
-          transition.matrices[i.treatment,i.sample,,]
-      }
-      
-      # Now use the cohort vectors to calculate the 
-      # total costs for each cycle
-      cycle.costs[i.treatment,i.sample,]<-
-        cohort.vectors[i.treatment,i.sample,,]%*%state.costs[i.sample,]
-      # And total QALYs for each cycle
-      cycle.qalys[i.treatment,i.sample,]<-
-        cohort.vectors[i.treatment,i.sample,,]%*%state.qalys[i.sample,]
-      
-      # Combine the cycle.costs and treatment.costs to get total costs
-      # Apply the discount factor 
-      # (1 in first year, 1.035 in second, 1.035^2 in third, and so on)
-      # Each year acounts for two cycles so need to repeat the discount values
-      total.costs[i.treatment,i.sample]<-treatment.costs[i.treatment,i.sample]+
-        cycle.costs[i.treatment,i.sample,]%*%
-        (1/1.035)^rep(c(0:(n.cycles/2-1)),each=2)
-      
-      # Combine the cycle.qalys to get total qalys
-      # Apply the discount factor 
-      # (1 in first year, 1.035 in second, 1.035^2 in third, and so on)
-      # Each year acounts for two cycles so need to repeat the discount values
-      total.qalys[i.treatment,i.sample]<-cycle.qalys[i.treatment,i.sample,]%*%
-        (1/1.035)^rep(c(0:(n.cycles/2-1)),each=2)
-    }
-  }
-  
   #############################################################################
   ## Analysis of results ######################################################
   #############################################################################
