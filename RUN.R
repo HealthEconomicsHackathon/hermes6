72c392f8a57803a194a0e469337745891e82db4a
mjkeng <mijun.keng@gmail.com>
2019-11-07 14:37:09 +0000

    rename var storing systime

--- RUN.R
+++ RUN.R
@@ -11,16 +11,16 @@ n.cycles <- 100
 n.samples <- 10000
 n.cores <- parallel::detectCores() - 2
 
-s1 <- system.time(markov(n.cycles = n.cycles, n.samples = n.samples)) 
-s2 <- system.time(markov_reduced_dimensions(n.cycles = n.cycles, n.samples = n.samples))
+stime_baseline <- system.time(markov(n.cycles = n.cycles, n.samples = n.samples)) 
+stime_reduceddim <- system.time(markov_reduced_dimensions(n.cycles = n.cycles, n.samples = n.samples))
 
 ## Vectorising
-s3 <- system.time(markov_vectorisetx(n.cycles = n.cycles, n.samples = n.samples))
-s5 <- system.time(markov_vectorisetxsmp(n.cycles = n.cycles, n.samples = n.samples))
+stime_vector_tx <- system.time(markov_vectorisetx(n.cycles = n.cycles, n.samples = n.samples))
+stime_vector_txsmp <- system.time(markov_vectorisetxsmp(n.cycles = n.cycles, n.samples = n.samples))
 
-s4 <- system.time(markov_expanded_vectorisesmp(n.states = n.states, n.cycles = n.cycles, n.samples = n.samples))
+stime_vector_smp <- system.time(markov_expanded_vectorisesmp(n.states = n.states, n.cycles = n.cycles, n.samples = n.samples))
 
 ## Parallelising 
-s6 <- system.time(markov_expanded_parallisesmp_foreach(n.states = n.states, n.cycles = n.cycles, n.samples = n.samples, n.cores = 2))
-s7 <- system.time(markov_expanded_parallisesmp_furrr(n.states = n.states, n.cycles = n.cycles, n.samples = n.samples))
+stime_parallel_foreach <- system.time(markov_expanded_parallisesmp_foreach(n.states = n.states, n.cycles = n.cycles, n.samples = n.samples, n.cores = 2))
+stime_parallel_furrr <- system.time(markov_expanded_parallisesmp_furrr(n.states = n.states, n.cycles = n.cycles, n.samples = n.samples))
 
