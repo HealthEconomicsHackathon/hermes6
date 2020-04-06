92e42c35923d3c3350c2ba682b337e5b29eb3264
Robert Ashton <r.ashton@imperial.ac.uk>
2020-04-06 23:16:22 +0100

    Fix Rcpp integration

--- R/markov_smoking_probabilistic_rcpp.R
+++ R/markov_smoking_probabilistic_rcpp.R
@@ -9,34 +9,6 @@
 #'
 #' @return Output
 #' @export
-
-r_for <- function(v, transition, n.cycles){
-  
-  for(i in 2:n.cycles)
-  {
-    # Markov update
-    # Multiply previous cycle's cohort vector by transition matrix
-    # i.e. pi_j = pi_(j-1)*P
-    v[i, ] <- v[i-1, ] %*% transition
-  }
-  return(v)
-}
-  
-Rcpp::cppFunction('NumericMatrix rcpp_loop(NumericMatrix mat_in, NumericMatrix transition, int n) {
-NumericMatrix out(mat_in.nrow(), transition.ncol());
-out(0, _) = mat_in(0, _);
-NumericVector rm1, cm2;
-for (int i = 1; i < n; i++) {
-  rm1 = out(i - 1,_);
-  for (size_t j = 0; j < transition.ncol(); ++j) {
-    cm2 = transition(_,j);
-    out(i,j) = std::inner_product(rm1.begin(), rm1.end(), cm2.begin(), 0.);              
-  }
-}
-return(out);
-}')
-
-
 markov_rcpp <- function() {
   set.seed(14143)
   
@@ -253,3 +225,14 @@ markov_rcpp <- function() {
   output
 }
 
+r_for <- function(v, transition, n.cycles){
+  
+  for(i in 2:n.cycles)
+  {
+    # Markov update
+    # Multiply previous cycle's cohort vector by transition matrix
+    # i.e. pi_j = pi_(j-1)*P
+    v[i, ] <- v[i-1, ] %*% transition
+  }
+  return(v)
+}
---
+++ src/rcpp_loop.cpp
@@ -0,0 +1,17 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [Rcpp::export]
+NumericMatrix rcpp_loop(NumericMatrix mat_in, NumericMatrix transition, int n) {
+  NumericMatrix out(mat_in.nrow(), transition.ncol());
+  out(0, _) = mat_in(0, _);
+  NumericVector rm1, cm2;
+  for (int i = 1; i < n; i++) {
+    rm1 = out(i - 1,_);
+    for (size_t j = 0; j < transition.ncol(); ++j) {
+      cm2 = transition(_,j);
+      out(i,j) = std::inner_product(rm1.begin(), rm1.end(), cm2.begin(), 0.);              
+    }
+  }
+  return(out);
+}
